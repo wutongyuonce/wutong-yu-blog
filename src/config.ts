38110110308dd88facb8c8bae21e4b7e8fedb523
wutongyuonce <147830929+wutongyuonce@users.noreply.github.com>
2026-08-14 20:31:34 +0800

    fix: use www domain in generated blog cards

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import type { Site, Ui, Features } from './types'
 
 export const SITE: Site = {
-  website: 'https://wutongyu.site/',
+  website: 'https://www.wutongyu.site/',
   base: '/',
   title: 'Wutong Yu',
   description:
